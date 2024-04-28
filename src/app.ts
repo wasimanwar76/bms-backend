805b83ec5f3d16f3b4190eb2809e7bc3345170af
Wasim Anwar <wasimsonu76@gmail.com>
2024-04-28 14:02:11 +0530

    Update Error Handler

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 import express from "express";
 import createHttpError from "http-errors";
-import globalErrorHandler from "./middlewares/globalErrorhandler.middleware";
+import globalErrorHandler from "./middlewares/errorHandler.middleware.";
 const app = express();
 
-app.get("", (req, res) => {
-  //   throw new Error("Something Went Wrong");
-  const error = createHttpError(400, "Something Went Wrong");
-  //   throw error;
-  //   res.json({ messaage: "Hello, How are you" });
-});
+// app.get("", (req, res) => {
+//   //   throw new Error("Something Went Wrong");
+//   const error = createHttpError(400, "Something Went Wrong");
+//   //   throw error;
+//   //   res.json({ messaage: "Hello, How are you" });
+// });
 
 //Global Error Handler
 app.use(globalErrorHandler);
